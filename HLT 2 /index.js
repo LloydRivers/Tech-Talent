b5a8c15e127905f836d5221cc358f55f117c535a
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-11-05 08:04:15 +0000

    .

--- HLT 2 /index.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("button");
-const popup = document.querySelector(".popup ")
-button.addEventListener("click", function(){
-     popup.style.display = "flex"
-})
-
-popup.addEventListener("click", function(){
-    popup.style.display = "none"
-})
